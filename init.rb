0c5942f489189eda19128af1394845e4a31b9acf
Adam Tanner <adam@adamtanner.org>
2010-09-28 15:23:34 -0400

    Update Finethread_RDiscount for Rails 3.

--- init.rb
+++ init.rb
@@ -1,5 +1,3 @@
-config.gem 'rdiscount'
-
 ActionController::Dispatcher.to_prepare :finethread_rdiscount do
   if PageBlock.respond_to?(:add_markup_behavior)
     PageBlock.add_markup_behavior FinethreadRdiscount
